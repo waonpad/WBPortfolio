383fd46b8bfe953025c940b1d97cce1980d4d677
waonpad <waonpad@gmail.com>
2023-01-24 22:12:06 +0900

    0124

---
+++ src/components/Theme.jsx
@@ -0,0 +1,24 @@
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { green, grey, yellow } from '@mui/material/colors';
+
+export const globalTheme = createTheme({
+    // palette: {
+    //     // mode: 'dark',
+    //     primary: {
+    //         main: green[400],
+    //         contrastText: '#fff'
+    //     },
+    //     background: {
+    //         default: '#f5f5f5',
+    //     },
+    // },
+    // components: {
+    //     MuiBackdrop: {
+    //         defaultProps: {
+    //             sx: {
+    //                 backgroundColor: '#f5f5f5',
+    //             }
+    //         }
+    //     }
+    // }
+});
